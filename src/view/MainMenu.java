ffd772441a44dd29de0d387b5d92f0e75f286eda
FSDavila <48636253+FSDavila@users.noreply.github.com>
2020-09-30 17:52:02 -0300

    Update MainMenu.java

--- src/view/MainMenu.java
+++ src/view/MainMenu.java
@@ -6,6 +6,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 
 import java.awt.event.ActionListener;
+import java.text.ParseException;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import java.awt.Font;
@@ -20,16 +21,20 @@ public class MainMenu extends JPanel {
 		setLayout(null);
 		
 		JButton btnNewButton_2 = new JButton("Cadastro de Funcionarios");
-		btnNewButton_2.setBounds(33, 70, 161, 23);
-		add(btnNewButton_2);
-		
-		JButton btnNewButton_3 = new JButton("Consulta de CDAs");
-		btnNewButton_3.setBounds(33, 104, 161, 23);
-		btnNewButton_3.addActionListener(new ActionListener() {
+		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				CadastroFuncionario telaCDA = null;
+				try {
+					telaCDA = new CadastroFuncionario();
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				telaCDA.setVisible(true);
 			}
 		});
-		add(btnNewButton_3);
+		btnNewButton_2.setBounds(36, 50, 161, 23);
+		add(btnNewButton_2);
 		
 		JButton btnNewButton = new JButton("Sair do Sistema");
 		btnNewButton.addActionListener(new ActionListener() {
@@ -37,7 +42,7 @@ public class MainMenu extends JPanel {
 				//System.exit(0); //sair do sistema
 			}
 		});
-		btnNewButton.setBounds(33, 138, 161, 23);
+		btnNewButton.setBounds(36, 185, 161, 23);
 		add(btnNewButton);
 		
 		JLabel lblNewLabel = new JLabel("Menu Principal");
@@ -45,15 +50,29 @@ public class MainMenu extends JPanel {
 		lblNewLabel.setBounds(69, 11, 93, 14);
 		add(lblNewLabel);
 		
-		JButton btnNewButton_1 = new JButton("Tela Prototipo Cad.");
+		JButton btnNewButton_1 = new JButton("Cadastrar CDAs");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				TelaCadContribuinte telaPrototype = new TelaCadContribuinte();
 				telaPrototype.setVisible(true);
 			}
 		});
-		btnNewButton_1.setBounds(33, 36, 161, 23);
+		btnNewButton_1.setBounds(36, 151, 161, 23);
 		add(btnNewButton_1);
+		
+		JButton btnNewButton_4 = new JButton("Consulta de CDAs");
+		btnNewButton_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				TelaConsultaCDA telaCDA = new TelaConsultaCDA();
+				telaCDA.setVisible(true);
+			}
+		});
+		btnNewButton_4.setBounds(36, 117, 161, 23);
+		add(btnNewButton_4);
+		
+		JButton btnNewButton_5 = new JButton("Cadastro de Contribuinte");
+		btnNewButton_5.setBounds(36, 83, 161, 23);
+		add(btnNewButton_5);
 
 	}
 }
