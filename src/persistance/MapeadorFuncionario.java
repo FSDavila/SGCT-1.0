daf74836327f124ed350c9d7179721f5f6371b70
FSDavila <48636253+FSDavila@users.noreply.github.com>
2020-09-30 21:38:15 -0300

    teste
    test

--- src/persistance/MapeadorFuncionario.java
+++ src/persistance/MapeadorFuncionario.java
@@ -23,7 +23,7 @@ public class MapeadorFuncionario{
     public Funcionario get(String CPF) {
         return cacheFuncionarios.get(CPF);
     }
-    public Funcionario get4(String CPF) {
+    public Funcionario get5(String CPF) {
         return cacheFuncionarios.get(CPF);
     }
     
