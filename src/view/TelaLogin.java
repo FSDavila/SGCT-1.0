4db4729156efebbf1a17dcc72bb485a24d9b4507
Bruno Alexandre <bruno_alexandre.s@hotmail.com>
2020-12-07 23:06:39 -0300

    Correções na tela Login

--- src/view/TelaLogin.java
+++ src/view/TelaLogin.java
@@ -11,6 +11,7 @@ import controller.ControllerContribuinte;
 import controller.ControllerFuncionario;
 import model.Contribuinte;
 import model.Funcionario;
+import persistance.MapeadorChatLog;
 
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -24,13 +25,15 @@ import javax.swing.JTextField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Label;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class TelaLogin extends JFrame {
 
 	private JPanel contentPane;
 	private JPasswordField passwordField;
 	private JTextField textField;
-	
+
 	private static TelaLogin instancia;
 
 	/**
@@ -48,13 +51,13 @@ public class TelaLogin extends JFrame {
 			}
 		});
 	}
-	
-    public static TelaLogin getInstancia() {
-        if (instancia == null) {
-            instancia = new TelaLogin();
-        }
-        return instancia;
-    }
+
+	public static TelaLogin getInstancia() {
+		if (instancia == null) {
+			instancia = new TelaLogin();
+		}
+		return instancia;
+	}
 
 	/**
 	 * Create the frame.
@@ -65,60 +68,80 @@ public class TelaLogin extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblNewLabel_3 = new JLabel("Criar conta de acesso ao chat");
+		lblNewLabel_3.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				setVisible(false);
+				TelaCadChatContribuinte.getInstancia().setVisible(true);
+			}
+		});
 		lblNewLabel_3.setHorizontalAlignment(SwingConstants.LEFT);
 		lblNewLabel_3.setForeground(Color.BLUE);
 		lblNewLabel_3.setBounds(10, 238, 226, 14);
 		contentPane.add(lblNewLabel_3);
-		
+
 		JButton btnNewButton = new JButton("Logar");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				Contribuinte cprocurado = ControllerContribuinte.getInstancia().getContribuinteByLogin(textField.getText());
-				Funcionario fprocurado = ControllerFuncionario.getInstancia().getFuncionarioByLogin(textField.getText());
-				if(fprocurado != null && cprocurado == null) {
-					JOptionPane.showMessageDialog(null, "Funcionario logado com sucesso!", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
-					TelaChatFuncionario.getInstancia().setFuncionariofuncionarioAtendente(fprocurado); //funcionario setado na janela de atendimento do chat para caso ele va atender
+				Contribuinte cprocurado = ControllerContribuinte.getInstancia()
+						.getContribuinteByLogin(textField.getText());
+				Funcionario fprocurado = ControllerFuncionario.getInstancia()
+						.getFuncionarioByLogin(textField.getText());
+				if (fprocurado != null && cprocurado == null) {
+					JOptionPane.showMessageDialog(null, "Funcionario logado com sucesso!", "Sucesso",
+							JOptionPane.INFORMATION_MESSAGE);
+					TelaChatFuncionario.getInstancia().setFuncionariofuncionarioAtendente(fprocurado); // funcionario setado na janela de atendimento do chat para caso ele vai atender
 					TelaMenuPrincipal.getInstancia().setVisible(true);
 					setVisible(false);
-				}
-				else if(cprocurado != null && fprocurado == null) {
-					//entrar no chat
-					JOptionPane.showMessageDialog(null, "Contribuinte logado com sucesso!", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
-				}
-				else {
-					JOptionPane.showMessageDialog(null, "Verifique os dados digitados e tente novamente.", "Aviso", JOptionPane.WARNING_MESSAGE);
+				} else if (cprocurado != null && fprocurado == null) {
+					// entrar no chat
+					JOptionPane.showMessageDialog(null, "Contribuinte logado com sucesso!", "Sucesso",
+							JOptionPane.INFORMATION_MESSAGE);
+					TelaChatContribuinte.getInstancia().setUsuarioLogado(cprocurado);// loga usuario no chat
+
+					if (MapeadorChatLog.getInstancia().get(cprocurado.getIdentificacao()) != null) {
+						TelaChatContribuinte.getInstancia().setDadosChat(cprocurado.getIdentificacao()); // carrega o historico do chat do usuario onde parou
+						
+					}
+					TelaChatContribuinte.getInstancia().setUsuarioOnline();
+					TelaChatContribuinte.getInstancia().setVisible(true);
+					setVisible(false);
+
+				} else {
+					JOptionPane.showMessageDialog(null, "Verifique os dados digitados e tente novamente.", "Aviso",
+							JOptionPane.WARNING_MESSAGE);
 				}
 			}
-			
+
 		});
 		btnNewButton.setBounds(79, 184, 89, 23);
 		contentPane.add(btnNewButton);
-		
+
 		passwordField = new JPasswordField();
 		passwordField.setBounds(43, 147, 154, 20);
 		contentPane.add(passwordField);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Sistema de Gest\u00E3o e Cobran\u00E7a Tributaria");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 9));
 		lblNewLabel_1.setBounds(31, 36, 180, 14);
 		contentPane.add(lblNewLabel_1);
-		
+
 		JLabel lblNewLabel = new JLabel("SGCT");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 11));
 		lblNewLabel.setBounds(103, 11, 29, 14);
 		contentPane.add(lblNewLabel);
-		
+
 		textField = new JTextField();
 		textField.setColumns(30);
 		textField.setBounds(43, 89, 154, 20);
 		contentPane.add(textField);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("Login:");
 		lblNewLabel_2.setBounds(44, 72, 46, 14);
 		contentPane.add(lblNewLabel_2);
-		
+
 		Label labelSenha = new Label("Senha:");
 		labelSenha.setBounds(42, 119, 62, 22);
 		contentPane.add(labelSenha);
